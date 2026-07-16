a79f3a30f4dd2d00b2b9a32a994c21369284ef63
Júlio Leal <91677208+Julio-Leal@users.noreply.github.com>
2026-07-16 11:53:22 +0000

    update: new updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,31 @@ const btn_copy_pix = document.getElementById("btn-copy-pix");
 
 btn_copy_pix.addEventListener("click", copiarChavePix);
 
-function copiarChavePix() {
-    navigator.clipboard.writeText(CONFIG.chavePix)
-        .then(() => {
-            alert("Chave PIX copiada com sucesso!");
-        })
-        .catch((erro) => {
-            console.error("Erro ao copiar: ", erro);
-        })
+// 🚩🚩🚩🚩🚩 OLD 🚩🚩🚩🚩🚩
+// function copiarChavePix() {
+//     navigator.clipboard.writeText(CONFIG.chavePix)
+//         .then(() => {
+//             alert("Chave PIX copiada com sucesso!");
+//         })
+//         .catch((erro) => {
+//             console.error("Erro ao copiar: ", erro);
+//         })
+// }
+// 🚩🚩🚩🚩🚩 OLD 🚩🚩🚩🚩🚩
+
+async function copiarChavePix() {
+    await navigator.clipboard.writeText(CONFIG.chavePix);
+
+    Swal.fire({
+        icon: 'success',
+        title: 'Chave PIX copiada!',
+        text: 'Agora é só colar no aplicativo do seu banco.',
+        confirmButtonText: 'OK',
+        confirmButtonColor: '#8B5E3C',
+        background: '#2b1d16',
+        color: '#fff'
+    });
 }
+
+
 
